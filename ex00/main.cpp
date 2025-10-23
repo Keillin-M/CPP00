43722e2e153543bc71e504d6302ef216a7340d27
Keillin Maeda <kmaeda@c3a9c3.42berlin.de>
2025-10-23 17:34:18 +0200

    ex00 finished

---
+++ ex00/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmaeda <kmaeda@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 16:47:24 by kmaeda            #+#    #+#             */
+/*   Updated: 2025/10/23 16:28:14 by kmaeda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				argv[i][j] = (char)toupper(argv[i][j]);
+			std::cout << argv[i];
+			if (argv[i + 1])
+				std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
