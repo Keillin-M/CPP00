e0249235f3ea30aacf84857e79856caccc8faceb
Keillin Maeda <kmaeda@c3a9c3.42berlin.de>
2025-10-23 17:52:53 +0200

    ex01 start hpp

---
+++ ex01/include/Contacts.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contacts.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmaeda <kmaeda@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/23 17:47:10 by kmaeda            #+#    #+#             */
+/*   Updated: 2025/10/23 17:52:26 by kmaeda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACTS.H
+# define CONTACTS.H
+
+class 
+{
+	public:
+	string	FirstName;
+	string	LastName;
+	string	NickName;
+	int	PhoneNumber;
+	string	DarkestSecret;
+}
+
+
+
+#endif
